d563d9e0573983f9cf7bf5eb89ba335b64d112fc
trankimthuong <thuongtk.bk@gmail.com>
2020-08-17 11:39:59 +0700

    jsp ung dung calculator

--- jsp_calculator/src/CalculatorServlet.java
+++ jsp_calculator/src/CalculatorServlet.java
@@ -1,10 +1,25 @@
-@javax.servlet.annotation.WebServlet(name = "CalculatorServlet")
-public class CalculatorServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@javax.servlet.annotation.WebServlet(name = "CalculatorServlet", urlPatterns = "/calculate")
+public class CalculatorServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        float firstOperand = Integer.parseInt(request.getParameter("first-operand"));
+        float secondOperand = Integer.parseInt(request.getParameter("second-operand"));
+        char operator = request.getParameter("operator").charAt(0);
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Result:</h1>");
+        try{
+            float result = Calculator.calculate(firstOperand, secondOperand, operator);
+            writer.println(firstOperand + " " + operator + " " + secondOperand + " = " + result);
+        }catch (Exception ex){
+            writer.println("Error: " + ex.getMessage());
+        }
+        writer.println("</html>");
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
 }
